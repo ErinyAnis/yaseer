58d039aa657838a51365b2e83f1cf7f34deba9bb
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2024-09-21 02:19:10 +0300

    update

--- components/others/ClientSlider.jsx
+++ components/others/ClientSlider.jsx
@@ -8,40 +8,39 @@ import { Swiper, SwiperSlide } from "swiper/react";
 export default function ClientSlider() {
   return (
     <Swiper
-  spaceBetween={10}  // Add some space between slides
-  slidesPerView={6}
-  breakpoints={{
-    1199: {
-      slidesPerView: 6,
-    },
-    768: {
-      slidesPerView: 4,
-    },
-    0: {
-      slidesPerView: 2,
-    },
-  }}
-  loop={false}  // Ensure loop is disabled to prevent duplication
-  watchSlidesProgress
-  modules={[Autoplay]}
-  autoplay={{ delay: 3000, disableOnInteraction: false }}  // Add autoplay delay
-  resizeObserver
-  className="small-item-carousel"
->
-  {clients.map((client) => (
-    <SwiperSlide key={client.id}>
-      <div className="logo-item">
-        <Image
-          src={client.imgSrc}
-          width="150"
-          height="90"
-          alt={client.name}
-          className="image-filter"
-        />
-      </div>
-    </SwiperSlide>
-  ))}
-</Swiper>
-
+      spaceBetween={10} // Add some space between slides
+      slidesPerView={6}
+      breakpoints={{
+        1199: {
+          slidesPerView: 6,
+        },
+        768: {
+          slidesPerView: 4,
+        },
+        0: {
+          slidesPerView: 2,
+        },
+      }}
+      loop={false} // Ensure loop is disabled to prevent duplication
+      watchSlidesProgress
+      modules={[Autoplay]}
+      autoplay={{ delay: 3000, disableOnInteraction: false }} // Add autoplay delay
+      resizeObserver
+      className="small-item-carousel"
+    >
+      {clients.map((client) => (
+        <SwiperSlide key={client.id}>
+          <div className="logo-item">
+            <Image
+              src={client.imgSrc}
+              width="150"
+              height="90"
+              alt={client.name}
+              className="image-filter"
+            />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
