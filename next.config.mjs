5a65fc96e30f66deeaf70a19cdbc4d150125713d
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2024-09-21 01:20:17 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-      locales: ['en', 'ar'], // Add your desired locales here
-      defaultLocale: 'en', // Set the default locale
-    },
-    eslint: {
-        ignoreDuringBuilds: true
-    }
+    
     // Other configurations can go here if needed
 };
 
