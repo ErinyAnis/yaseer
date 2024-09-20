c860370b4ada9f85e92bb5b57012fc86cb948036
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2024-09-21 00:39:29 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     i18n: {
-      locales: ['en', 'ar'], // English and Arabic
-      defaultLocale: 'en',
+      locales: ['en', 'ar'], // Add your desired locales here
+      defaultLocale: 'en', // Set the default locale
     },
+    // Other configurations can go here if needed
   };
   
   export default nextConfig;
